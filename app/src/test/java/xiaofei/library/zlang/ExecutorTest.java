13a20a1b23e5dee0cd77cae02af17739f3ddcdf0
Xiaofei <xiaofei.it@gmail.com>
2017-12-12 21:34:48 +0800

    Remove out-of-date test cases

--- app/src/test/java/xiaofei/library/zlang/ExecutorTest.java
+++ app/src/test/java/xiaofei/library/zlang/ExecutorTest.java
@@ -39,14 +39,14 @@ public class ExecutorTest {
         System.out.println(library.execute("f3", new Object[]{100}));
     }
 
-    @Test
-    public void test2() throws Exception {
-        Library library = new Library.Builder()
-                .addFunctions("function f(a) {b = _test_add(a, a - 1); return _test_add(b, a/3, a);}")
-                .build();
-        library.print("f", 1);
-        System.out.println(library.execute("f", new Object[]{100}));//199 + 100 + 33
-    }
+//    @Test
+//    public void test2() throws Exception {
+//        Library library = new Library.Builder()
+//                .addFunctions("function f(a) {b = _test_add(a, a - 1); return _test_add(b, a/3, a);}")
+//                .build();
+//        library.print("f", 1);
+//        System.out.println(library.execute("f", new Object[]{100}));//199 + 100 + 33
+//    }
 
     @Test
     public void test3() throws Exception {
@@ -64,31 +64,31 @@ public class ExecutorTest {
         System.out.println(library.execute("f", new Object[]{false}));
     }
 
-    private static final class TestA {
-        String f(String s) {
-            return "s=" + s;
-        }
-    }
-    @Test
-    public void test4() {
-        Library library = new Library.Builder()
-                .addFunctions(
-                        "function new_instance(className) {" +
-                                "class = _for_name(className); if (class == null) return null;" +
-                                "cons = _class_get_declared_constructor(class);" +
-                                "return _constructor_new_instance(cons);}")
-                .addFunctions(
-                        "function call_function(className) {" +
-                                "class = _for_name(className);" +
-                                "object = new_instance(className);" +
-                                "method = _class_get_declared_method(class, \"f\", _for_name(\"java.lang.String\"));" +
-                                "return _method_invoke(method, object, \"kkk\");}"
-                )
-                .build();
-        library.print("new_instance", 1);
-        System.out.println(library.execute("new_instance", new Object[]{TestA.class.getName()}));
-        System.out.println(library.execute("call_function", new Object[]{TestA.class.getName()}));
-    }
+//    private static final class TestA {
+//        String f(String s) {
+//            return "s=" + s;
+//        }
+//    }
+//    @Test
+//    public void test4() {
+//        Library library = new Library.Builder()
+//                .addFunctions(
+//                        "function new_instance(className) {" +
+//                                "class = _for_name(className); if (class == null) return null;" +
+//                                "cons = _class_get_declared_constructor(class);" +
+//                                "return _constructor_new_instance(cons);}")
+//                .addFunctions(
+//                        "function call_function(className) {" +
+//                                "class = _for_name(className);" +
+//                                "object = new_instance(className);" +
+//                                "method = _class_get_declared_method(class, \"f\", _for_name(\"java.lang.String\"));" +
+//                                "return _method_invoke(method, object, \"kkk\");}"
+//                )
+//                .build();
+//        library.print("new_instance", 1);
+//        System.out.println(library.execute("new_instance", new Object[]{TestA.class.getName()}));
+//        System.out.println(library.execute("call_function", new Object[]{TestA.class.getName()}));
+//    }
 
     @Test
     public void test5() {
